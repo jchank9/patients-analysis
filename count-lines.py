605332c6feac5908efaa49fe9bb4b02f171042f5
jchank9 <jchan.kim1@gmail.com>
2021-11-18 16:09:42 +0100

    Modify the print message

--- count-lines.py
+++ count-lines.py
@@ -1,12 +1,9 @@
-@@ -1,3 +1,6 @@
-
 """This module counts the number of lines
 in standard input.
-Input: A string from the system's standard input
-Output: A string with the total number of lines"""
+Input: A string from the system's standard input"""
 
 import sys
 count=0
 for line in sys.stdin:
 	count+=1
-print(count,"lines in standard input")
+print("We found",count,"lines in standard input")
